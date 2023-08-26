7570c33482d6bc4cbfd6de3ff65d265cfcbfe6aa
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-26 15:06:51 +0200

    modify courses service not to let do more than one http request using shareReplay create skeleton of courses card list component

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.scss']
+})
+export class CoursesCardListComponent {
+
+}
