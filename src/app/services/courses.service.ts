db4c16d519dabb56695fa8d1e4df02681e58b3d0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-30 14:06:28 +0200

    introduction to single data observable pattern

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -17,6 +17,20 @@ export class CoursesService {
       .pipe(shareReplay());
   }
 
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          pageSize: "10000",
+          courseId: courseId.toString(),
+        },
+      })
+      .pipe(
+        map((response) => response["payload"]),
+        shareReplay()
+      );
+  }
+
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses").pipe(
       map((res) => res["payload"]),
