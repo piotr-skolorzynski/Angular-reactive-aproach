4cd0ace07e8511ca4190e03209e026e43443e225
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-30 10:52:41 +0200

    auth store - browser refresh support with local storage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data";
+
 @Injectable({
   providedIn: "root",
 })
@@ -15,18 +17,27 @@ export class AuthStore {
   isLoggedOut$: Observable<boolean>;
 
   constructor(private http: HttpClient) {
+    const user = localStorage.getItem(AUTH_DATA);
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
+
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((isLoggedIn) => !isLoggedIn));
   }
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>("/api/login", { email, password }).pipe(
-      tap((user) => this.subject.next(user)),
+      tap((user) => {
+        this.subject.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
 
   logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 }
